14ab1c6973aced0f70c828d656608416a195337c
MDrschl <117458298+MDrschl@users.noreply.github.com>
2025-05-10 19:00:47 +0200

    Initial push

--- dataCleaning.py
+++ dataCleaning.py
@@ -2,4 +2,14 @@ import pandas as pd
 import os
 print(os.getcwd())
 
-btc_price = pd.read_csv("BTCUSDT_1m.csv")
+dfBTC = pd.read_csv("data/BTCUSDT_1m.csv")
+dfETH = pd.read_csv("data/ETHUSDT_1m.csv")
+print("BTC Time Range:")
+print("Start:", dfBTC["Open Time"].iloc[0])
+print("End:  ", dfBTC["Open Time"].iloc[-1])
+print(dfBTC.head())
+
+print("\nETH Time Range:")
+print("Start:", dfETH["Open Time"].iloc[0])
+print("End:  ", dfETH["Open Time"].iloc[-1])
+print(dfETH.head())
