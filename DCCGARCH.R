9543763f44a443f7803f4d0b96d166be9adf4cdc
MDrschl <117458298+MDrschl@users.noreply.github.com>
2025-05-13 13:23:32 +0200

    DCC

--- DCCGARCH.R
+++ DCCGARCH.R
@@ -5,3 +5,21 @@ library(rmgarch)
 # Load data
 returns <- read_csv("train_returns.csv")
 returns_xts <- xts(returns[, -1], order.by = as.Date(returns[[1]]))
+
+
+# Define univariate GARCH specification (same for both series)
+uspec <- ugarchspec(
+  variance.model = list(model = "sGARCH", garchOrder = c(1, 1)),
+  mean.model = list(armaOrder = c(0, 0)),
+  distribution.model = "std"
+)
+
+# Combine into a multivariate DCC-GARCH spec
+spec <- dccspec(
+  uspec = multispec(replicate(2, uspec)),
+  dccOrder = c(1, 1),
+  distribution = "mvnorm"
+)
+
+# Fit the model
+fit <- dccfit(spec, data = returns_xts)
